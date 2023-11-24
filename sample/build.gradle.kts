ce1cd33154fb61911a0fe790841171b05b682b4c
raycoarana <me@raycoarana.com>
2023-11-24 01:50:58 +0100

    Remove extra compose dep

--- sample/build.gradle.kts
+++ sample/build.gradle.kts
@@ -48,5 +48,4 @@ dependencies {
 
   androidTestImplementation("androidx.test.ext:junit:1.1.5")
   androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-  androidTestImplementation("androidx.compose.ui:ui-test-junit4")
 }
